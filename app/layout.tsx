68a50dff09f842de1bef23d40778a9d2ff6e9e42
Mangesh98 <mangeshraje55555@gmail.com>
2024-02-05 14:19:39 +0530

    Assets added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Work_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+import { Room } from "./Room";
+
 
 export const metadata = {
 	title: "Figma Clone",
@@ -10,14 +11,21 @@ export const metadata = {
 		"A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
 };
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
-}
+const workSans = Work_Sans({
+	subsets: ["latin"],
+	variable: "--font-work-sans",
+	weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+	<html lang="en">
+		<body className={`${workSans.className} bg-primary-grey-200`}>
+			<Room>
+				{children}
+				{/* <TooltipProvider>{children}</TooltipProvider> */}
+			</Room>
+		</body>
+	</html>
+);
+
+export default RootLayout;
