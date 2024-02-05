5cf4f64a32443645783993cec809ebd00cc2602e
Mangesh98 <mangeshraje55555@gmail.com>
2024-02-05 14:04:38 +0530

    File Structure  added Shadcn

--- app/CollaborativeApp.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useOthers } from "@/liveblocks.config";
-
-export default function CollaborativeApp() {
-	const others = useOthers();
-	const useCount = others.length;
-	return <h1>There are {useCount} other user(s) online</h1>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import CollaborativeApp from "./CollaborativeApp";
 import { Room } from "./Room";
 
 export default function Page() {
 	return (
 		<Room>
-			<CollaborativeApp />
+			<h1>Main page</h1>
 		</Room>
 	);
 }
